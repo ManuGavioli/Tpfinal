09da181710fd824e407551d9c18cef2a6815b1b8
Romma711 <74219542+Romma711@users.noreply.github.com>
2021-06-10 17:35:13 -0300

    Commit 6 Roma Cargar items en disco y Libro, en disco solo falta hacer la carga del Local Date y del array

--- src/com/utn/items/Disco.java
+++ src/com/utn/items/Disco.java
@@ -5,6 +5,7 @@ import com.utn.items.enums.GenerosD;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Disco extends itemVenta{
@@ -22,7 +23,7 @@ public class Disco extends itemVenta{
     public Disco(float precio, String nombre, ClasificacionEdad clasificacion, String solo_Banda, ArrayList<String> canciones, GenerosD genero, LocalDate fechaLanzamiento) {
         super(precio, nombre, clasificacion);
         Solo_Banda = solo_Banda;
-        Canciones = canciones;
+        Canciones= new ArrayList<String>();
         Genero = genero;
         FechaLanzamiento = fechaLanzamiento;
     }
@@ -44,6 +45,60 @@ public class Disco extends itemVenta{
 
     @Override
     public void CargarItems() {
+        //Se carga un nuevo item al programa
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del disco: ");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Ingrese el precio del producto: ");
+        float precio = scanner.nextFloat();
+
+        System.out.println("Ingrese el nombre de la banda o del solista: ");
+        String soloBanda = scanner.nextLine();
+
+        //
+        //Aca iria Añadir canciones
+        //
+
+        System.out.println("Ingrese la clasificacion de edad del disco: \s" +
+                "1 - Niños." +
+                "2 - Adolecentes." +
+                "3 - Adultos.");
+        int edad = scanner.nextInt();
+        ClasificacionEdad clasEdad = null;
+        switch (edad) {
+            case 1 -> clasEdad = ClasificacionEdad.NINIOS;
+            case 2 -> clasEdad = ClasificacionEdad.ADOLECENTES;
+            case 3 -> clasEdad = ClasificacionEdad.ADULTOS;
+            default -> System.out.println("Opcion no valida. Reintente");
+        }
+
+        System.out.println("Ingrese el genero del disco: \s" +
+                "1 - Rock." +
+                "2 - Pop." +
+                "3 - Jazz." +
+                "4 - Electronica." +
+                "5 - Clasica." +
+                "6 - Blues." +
+                "7 - R&B." +
+                "8 - Hip Hop.");
+        int gen = scanner.nextInt();
+        GenerosD genderD = null;
+        switch (gen){
+            case 1 -> genderD = GenerosD.ROCK;
+            case 2 -> genderD = GenerosD.POP;
+            case 3 -> genderD = GenerosD.JAZZ;
+            case 4 -> genderD = GenerosD.ELECTRONICA;
+            case 5 -> genderD = GenerosD.CLASICA;
+            case 6 -> genderD = GenerosD.BLUES;
+            case 7 -> genderD = GenerosD.RandB;
+            case 8 -> genderD = GenerosD.HIP_HOP;
+            default -> System.out.println("Opcion no valida. Reintente");
+        }
+
+        //
+        //Aca va el local Date
+        //
 
     }
 
--- src/com/utn/items/Libro.java
+++ src/com/utn/items/Libro.java
@@ -3,6 +3,7 @@ package com.utn.items;
 import com.utn.items.enums.ClasificacionEdad;
 import com.utn.items.enums.GenerosL;
 
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Libro extends itemVenta{
@@ -50,7 +51,61 @@ public class Libro extends itemVenta{
 
     @Override
     public void CargarItems() {
+        //Aca se carga un item al listado
+        Scanner scanner = new Scanner (System.in);
 
+        System.out.println("Ingrese El nombre del libro: ");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Ingrese el nombre de la editorial: ");
+        String editorial = scanner.nextLine();
+
+        System.out.println("Ingrese el nombre del/la autor/autora: ");
+        String autor = scanner.nextLine();
+
+        System.out.println("Ingrese el precio del producto: ");
+        float precio = scanner.nextFloat();
+
+        System.out.println("""
+                Ingrese la clasificacion de edad del juego de mesa:\s
+                1 - Niños.
+                2 - Adolecentes.
+                3 - Adultos.
+                """);
+        int Edad = scanner.nextInt();
+        ClasificacionEdad clasEdad = null;
+        switch (Edad) {
+            case 1 -> clasEdad = ClasificacionEdad.NINIOS;
+            case 2 -> clasEdad = ClasificacionEdad.ADOLECENTES;
+            case 3 -> clasEdad = ClasificacionEdad.ADULTOS;
+            default -> System.out.println("Opcion no valida. Reintente");
+        }
+
+        System.out.println("Ingrese el genero del libro: \s" +
+                "1 - Accion." +
+                "2 - Aventura." +
+                "3 - Romance." +
+                "4 - Drama." +
+                "5 - Poesia." +
+                "6 - Medieval." +
+                "7 - Ciencia ficcion." +
+                "8 - Fantasia." +
+                "9 - Policial");
+        int gen = scanner.nextInt();
+        GenerosL generL = null;
+        switch (gen){
+            case 1 -> generL = GenerosL.ACCION;
+            case 2 -> generL = GenerosL.AVENTURA;
+            case 3 -> generL = GenerosL.ROMANCE;
+            case 4 -> generL = GenerosL.DRAMA;
+            case 5 -> generL = GenerosL.POESIA;
+            case 6 -> generL = GenerosL.MEDIEVAL;
+            case 7 -> generL = GenerosL.CIENCIA_FICCION;
+            case 8 -> generL = GenerosL.FANTASIA;
+            case 9 -> generL = GenerosL.POLICIAL;
+            default -> System.out.println("Opcion no valida. Reintente");
+        }
+        Libro libro = new Libro(precio,nombre,clasEdad,generL,autor,editorial);
     }
 
     @Override
